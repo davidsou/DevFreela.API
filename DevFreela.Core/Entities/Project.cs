e3a747d0ec75ec758f3293069d7881550bac0641
David Oliveira <90005502@pmenos.com.br>
2023-08-22 17:55:51 -0300

    EntityFramework.

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -12,7 +12,7 @@ namespace DevFreela.Core.Entities
             IdFreelancer = idFreelancer;
             TotalCost = totalCost;
 
-            Status = ProjectStatusEnum.Created; 
+            Status = ProjectStatusEnum.Created;
             CreatedAt = DateTime.Now;
             Comments = new List<ProjectComment>();
 
@@ -21,9 +21,11 @@ namespace DevFreela.Core.Entities
         public string Title { get; private set; }
         public string Description { get; private set; }
         public int IdClient { get; private set; }
-
+        public User Client { get; private set; }
         public int IdFreelancer { get; private set; }
-        public decimal? TotalCost { get;private set; }
+        public User Freelancer { get; private set; }
+
+        public decimal? TotalCost { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? StartedAt { get; private set; }
         public DateTime? FinishedAt { get; private set; }
@@ -34,7 +36,7 @@ namespace DevFreela.Core.Entities
 
         public void Cancel()
         {
-            if(Status == ProjectStatusEnum.InProgress || Status == ProjectStatusEnum.Created)
+            if (Status == ProjectStatusEnum.InProgress || Status == ProjectStatusEnum.Created)
             {
                 Status = ProjectStatusEnum.Cancelled;
             }
--- DevFreela.Core/Entities/ProjectComment.cs
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -12,7 +12,11 @@
         public string Content { get; private set; }
 
         public int IdProject { get; private set; }
+
+        public Project  Project { get; private set; }
         public int IdUser { get; private set; }
+
+        public User User { get; private set; }
 
     }
 }
